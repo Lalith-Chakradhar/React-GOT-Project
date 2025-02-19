3ca0b8968479f40382d1a5ec8f50be596d836697
Lalith Chakradhar <lalith.rangana.31.3@mountblue.tech>
2025-02-19 17:22:47 +0530

    Made the changes in the filter tab search and text based search

--- src/components/AllButtons.jsx
+++ src/components/AllButtons.jsx
@@ -6,7 +6,9 @@ import got from '../../data';
 const AllButtons = ({selectedFilter, setSelectedFilter, setSearchText}) => {
   return (
     <section className='flex flex-wrap gap-2 justify-center mb-16'>
-        {got.houses.map((eachHouse)=> <Button houseName={eachHouse.name} selectedFilter={selectedFilter} setSelectedFilter={setSelectedFilter} setSearchText={setSearchText}/>)}
+        {got.houses.map((eachHouse)=> {
+        return <Button houseName={eachHouse.name} selectedFilter={selectedFilter} setSelectedFilter={setSelectedFilter} setSearchText={setSearchText}/>
+        })}
     </section>
   )
 }
